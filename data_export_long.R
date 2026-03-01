abc14010aae7a3514222f32826082bd4273c318c
DomecStoces <dominik.stoces@mendelu.cz>
2026-03-01 20:43:31 +0100

    adding columns with traits for each species to existing long format dataset

--- data_export_long.R
+++ data_export_long.R
@@ -32,3 +32,32 @@ write_xlsx(all_data_long, "Hamřík_long.xlsx")
 
 dataset_kula<-read_excel("Hamřík_long.xlsx")
 table(dataset_kula$year,dataset_kula$site,dataset_kula$site_protection)
+
+# Adding traits info to taxon_groups
+traits_path <- "traits.xlsx"
+
+# INFO: spiders are not yet done
+trait_sheets <- excel_sheets(traits_path)
+trait_sheets <- trait_sheets[!trait_sheets %in% c("Pavouci")]
+all_traits <- trait_sheets %>%
+  set_names() %>% 
+  map_df(~{
+    sheet_data <- read_excel(traits_path, sheet = .x) %>%
+      clean_names() 
+    
+    sheet_data %>%
+      rename(species = 1) %>% 
+      mutate(taxon_group = .x) %>%
+      mutate(species = as.character(species)) %>%
+      mutate(species = tolower(species)) %>% 
+      mutate(species = str_replace_all(species, " ", "_"))
+  })
+final_data <- all_data_long %>%
+  left_join(
+    all_traits, 
+    by = c("Species" = "species", "taxon_group" = "taxon_group") 
+  )
+
+glimpse(final_data)
+View(final_data)
+write_xlsx(final_data, "Hamřík_long_traits.xlsx")
